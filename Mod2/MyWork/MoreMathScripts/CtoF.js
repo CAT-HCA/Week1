2398fc030d94cac7ecd5225eca83ab8b853115c5
Cori <corinne.a.ader@gmail.com>
2019-07-26 09:14:46 -0400

    updated syntax and comments on past projects

--- Mod2/MyWork/MoreMathScripts/CtoF.js
+++ Mod2/MyWork/MoreMathScripts/CtoF.js
@@ -1,6 +1,10 @@
+// Description: Distance Formula
+// This script will calculate the distance between 2 points
+//Author: Corinne Trudeau
 "use strict";
-
+//User Input
 var tempCel = 20;
+//Calculations
 var tempFah = tempCel * 1.8 + 32
-
+//Output
 console.log(tempCel + " degrees Celsius converts to " + tempFah + " degrees Fahrenheit")
--- Mod2/MyWork/MoreMathScripts/rentals.js
+++ Mod2/MyWork/MoreMathScripts/rentals.js
@@ -1,23 +1,26 @@
+// Description: Van Rentals
+// This script can be used to calculate how man vans would be needed and cost (total and per person)
+//Author: Corinne Trudeau
 "use strict";
 
+//User Input
 var vanPrice = 250;
 var people = 38;
+
+//Calculations
 var vans = Math.ceil(people/15)
 var vanCost = vans * vanPrice
-
-console.log("You would need " + vans + " vans for " + people +" passengers.")
-
 var indCost = vanCost / people;
-indCost = parseFloat(indCost.toFixed(2));
-
-console.log("If there are " + people + " passengers, each person would owe $" + indCost) 
-
 var cashRec = indCost * people; 
-cashRec = parseFloat(cashRec.toFixed(2));
-
-console.log("If each passenger pays $" + indCost + ", you would collect a total of $" + cashRec)
-
 var extraMoney = cashRec - vanCost;
+
+//Variables
+indCost = parseFloat(indCost.toFixed(2));
+cashRec = parseFloat(cashRec.toFixed(2));
 extraMoney = parseFloat(extraMoney.toFixed(2));
 
+//Output
+console.log("You would need " + vans + " vans for " + people +" passengers.")
+console.log("If there are " + people + " passengers, each person would owe $" + indCost) 
+console.log("If each passenger pays $" + indCost + ", you would collect a total of $" + cashRec)
 console.log("You received an extra $" + extraMoney + " by collecting " + indCost + " per person.")
