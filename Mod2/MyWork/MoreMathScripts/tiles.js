d3f3b265b086751e653742587112b15aaa1c949f
Cori <corinne.a.ader@gmail.com>
2019-07-26 22:35:38 -0400

    went back and added some comments to previous files

--- Mod2/MyWork/MoreMathScripts/tiles.js
+++ Mod2/MyWork/MoreMathScripts/tiles.js
@@ -1,12 +1,21 @@
+// Description: This script calculates the number of tiles needed to tile a room
+// with user input of floor dimensions, it will calculate number of tiles needed including an extra 10%
+//for in case of damage, and ultimately the number of boxes of tiles needed.
+//Author: Corinne Trudeau
 "use strict";
 
+//User Input
 var floorLength = 3;
 var floorWidth = 8;
+
+//Variables
 var tiles = floorLength * floorWidth;
-var extraTiles = Math.ceil(tiles * 1.1)
 
-console.log(extraTiles)
+//Calculations
+var extraTiles = Math.ceil(tiles * 1.1);
+var boxes = Math.ceil(extraTiles/12);
 
-var boxes = Math.ceil(extraTiles/12)
+//Output
+console.log("To tile a room with dimensions " + floorLength + " feet by " + floorWidth + " feet, you would need exactly " + tiles + " tiles.  To add an extra 10% in case of damage, you would need a total of " + extraTiles + " tiles, which would require " + boxes + " boxes." );
 
-console.log("You would need " + boxes + " boxes to have 10% extra.")
+
